bf5f4742d0004811cf6aca2a99c6e3b8add61e0c
Matheus Lima <matheusldasilva2008@gmail.com>
2020-04-07 10:03:53 -0300

    adiciona sprites do player e animações

--- Player.gd
+++ Player.gd
@@ -8,10 +8,15 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
 		motion.x = SPEED
+		$Sprite.play("Run")
+		$Sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
+		$Sprite.play("Run")
+		$Sprite.flip_h = true
 	else:
 		motion.x = 0
+		$Sprite.play("Idle")
 		
 	if is_on_floor():
 		print("está no chao")
