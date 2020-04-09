2ec67e6fc7d63d57d775ee0b79016dd14b8b0cbf
Matheus Lima <matheusldasilva2008@gmail.com>
2020-04-09 16:04:43 -0300

    verifica se o corpo encostou na saida

--- Passagem.gd
+++ Passagem.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Passagem_body_entered(body):
+	print("entrou")
